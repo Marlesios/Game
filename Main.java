08b55545c0352c3d0bd501299749af8059014ce4
Marlesios <marles.nuralyev@gmail.com>
2022-06-11 03:35:08 +0300

    found solution for zip

--- Main.java
+++ Main.java
@@ -105,7 +105,19 @@ public class Main {
         dat.add("E://Games1/savegames/saveSecond.dat");
         dat.add("E://Games1/savegames/saveThird.dat");
 
-        makeZip("E://Games1/savegames/zip.zip",dat);
+//        makeZip("E://Games1/savegames/zip.zip","E://Games1/savegames/saveFirst.dat");
+
+        try(ZipOutputStream fos = new ZipOutputStream(new FileOutputStream("E://Games1/savegames/zip.zip"));
+        FileInputStream zos = new FileInputStream("E://Games1/savegames/saveFirst.dat")){
+            ZipEntry entry = new ZipEntry("saveFirst.dat");
+            fos.putNextEntry(entry);
+            byte[] buffer1 = new byte[zos.available()];
+            zos.read(buffer1);
+            fos.write(buffer1);
+            fos.closeEntry();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
     public static String makeDirectory(File file) {
         if (!file.exists()) {
@@ -137,19 +149,20 @@ public class Main {
         }
     }
     //Метод для архивации файлов ZIP
-    public static void makeZip(String str,List list){
-        for(int i=0; i<list.size();i++){
-        try(ZipOutputStream fos = new ZipOutputStream(new FileOutputStream(str));
-        FileInputStream oos = new FileInputStream((String) list.get(i))){
-            ZipEntry entry = new ZipEntry((String) list.get(i));
-            byte[] buffer = new byte[oos.available()];
-            fos.write(buffer);
-            fos.closeEntry();
+//    public static void makeZip(String str,String list){
+////        for(int i=0; i<list.size();i++){
+//        try(ZipOutputStream fos = new ZipOutputStream(new FileOutputStream(str));
+//        FileInputStream oos = new FileInputStream(list)){
+//            ZipEntry entry = new ZipEntry(list);
+//            fos.putNextEntry(entry);
+//            byte[] buffer = new byte[oos.available()];
+//            fos.write(buffer);
+//            fos.closeEntry();
+//
+//        }catch (Exception ex){
+//            System.out.println(ex.getMessage());
+////        }
 
-        }catch (Exception ex){
-            System.out.println(ex.getMessage());
-        }
-        }
-
-    }
+//    }
+//    }
 }
